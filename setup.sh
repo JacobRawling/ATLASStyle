bd241ad73cb1db9f06905d8ac247cb6dacc8de70
JacobRawling <mrjacob3@gmail.com>
2017-10-13 11:28:27 +0100

    Added style and setup scripts.

---
+++ setup.sh
@@ -0,0 +1,4 @@
+echo "export ATLAS_STYLE_PATH=$PWD" >> ~/.bash_profile
+export ATLAS_STYLE_PATH=$PWD
+echo "export PYTHON_PATH=$ATLAS_STYLE_PATH:$PYTHON_PATH" >> ~/.bash_profile
+export PYTHON_PATH=$ATLAS_STYLE_PATH:$PYTHON_PATH
